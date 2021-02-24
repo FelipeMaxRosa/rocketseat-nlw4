db2ed061bb4ea438f8f38400e0db9f11a82cfe02
felipemaxrosa <felipemax.suporteti@gmail.com>
2021-02-24 10:25:11 -0300

    feat: challengebox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import ExperienceBar from "../components/ExperienceBar";
 import Head from 'next/head';
-import Profile from "../components/Profile";
 import styles from "../styles/pages/Home.module.css";
+
+import ExperienceBar from "../components/ExperienceBar";
+import Profile from "../components/Profile";
 import CompletedChallenges from "../components/CompletedChallenges";
 import Countdown from "../components/Countdown";
+import ChallengeBox from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -20,7 +22,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
